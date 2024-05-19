c84ea12b3d6768c524a6c3cffde1149c79220d90
oygurd <86951294+oygurd@users.noreply.github.com>
2024-05-19 17:58:09 +0300

    new movement system, needs improving!

--- Assets/_Player/movement/CameraController.cs
+++ Assets/_Player/movement/CameraController.cs
@@ -4,87 +4,13 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-
-    public Camera mainCamera;
-    public Camera weaponCamera;
-    float sensX = 1f;
-    float sensY = 1f;
-    float baseFov = 90f;
-    float maxFov = 140f;
-    float wallRunTilt = 15f;
-
-    float wishTilt = -90;
-    float curTilt = 90;
-    Vector2 currentLook;
-    Vector2 sway = Vector3.zero;
-    float fov;
-    Rigidbody rb;
-
-    void Start()
-    {
-        rb = GetComponentInParent<Rigidbody>();
-        curTilt = transform.localEulerAngles.z;
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
-    }
+    public float mouseSensitivity = 100f; // Adjust sensitivity as needed
 
     void Update()
     {
-        RotateMainCamera();
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        transform.localRotation = Quaternion.Euler(-mouseY, mouseX, 0); // Rotate around Y-axis for horizontal view and X-axis for vertical view
     }
-
-    void FixedUpdate()
-    {
-        float addedFov = rb.velocity.magnitude - 3.44f;
-        fov = Mathf.Lerp(fov, baseFov + addedFov, 0.5f);
-        fov = Mathf.Clamp(fov, baseFov, maxFov);
-        mainCamera.fieldOfView = fov;
-        weaponCamera.fieldOfView = fov;
-
-        currentLook = Vector2.Lerp(currentLook, currentLook + sway, 0.8f);
-        curTilt = Mathf.LerpAngle(curTilt, wishTilt * wallRunTilt, 0.05f);
-
-        sway = Vector2.Lerp(sway, Vector2.zero, 0.2f);
-    }
-
-    void RotateMainCamera()
-    {
-        Vector2 mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
-        mouseInput.x *= sensX;
-        mouseInput.y *= sensY;
-
-        currentLook.x += mouseInput.x;
-        currentLook.y = Mathf.Clamp(currentLook.y += mouseInput.y, -90, 90);
-
-        transform.localRotation = Quaternion.AngleAxis(-currentLook.y, Vector3.right);
-        transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, curTilt);
-        transform.root.transform.localRotation = Quaternion.Euler(0, currentLook.x, 0);
-    }
-
-    public void Punch(Vector2 dir)
-    {
-        sway += dir;
-    }
-
-    #region Setters
-    public void SetTilt(float newVal)
-    {
-        wishTilt = newVal;
-    }
-
-    public void SetXSens(float newVal)
-    {
-        sensX = newVal;
-    }
-
-    public void SetYSens(float newVal)
-    {
-        sensY = newVal;
-    }
-
-    public void SetFov(float newVal)
-    {
-        baseFov = newVal;
-    }
-    #endregion
 }
--- Assets/_Player/movement/newPlayerMovement.cs
+++ Assets/_Player/movement/newPlayerMovement.cs
@@ -5,10 +5,11 @@ using UnityEngine.InputSystem;
 
 public class newPlayerMovement : MonoBehaviour
 {
-   
+    CameraController camerascript;
+
     [Header("Rigidbody settings")]
     //rigidbody part
-    Rigidbody PlayerRb;
+    [SerializeField] Rigidbody PlayerRb;
     [SerializeField] float Mass;
     [SerializeField] float Drag;
     [SerializeField] float AngularDrag;
@@ -21,43 +22,60 @@ public class newPlayerMovement : MonoBehaviour
     //-----------------------------------------------//
     [Header("Camera settings")]
     public Camera mainCam;
-    
-
+    public Transform camRot;
+    private Vector3 rotcam;
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        PlayerRb = GetComponent<Rigidbody>();
+        //PlayerRb = GetComponent<Rigidbody>();
         PlayerRb.mass = Mass;
         PlayerRb.drag = Drag;
         PlayerRb.angularDrag = AngularDrag;
         PlayerRb.useGravity = Gravity;
 
+
         playerInput = GetComponent<PlayerInput>();
         moveAction = playerInput.actions.FindAction("Move");
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        //transform.rotation = Quaternion.LookRotation(mainCam.transform.forward, mainCam.transform.up);
+        //this.transform.localRotation = Quaternion.Euler(0 , camRot.transform.rotation.eulerAngles.y, 0);
+        //transform.Rotate(Vector3.up * camRot.rotation.x);
         //Movement
         Move();
-        
+
     }
 
     void Move()
     {
-        Vector2 direction = moveAction.ReadValue<Vector2>();
+        Vector3 direction = moveAction.ReadValue<Vector3>();
         // transform.position += new Vector3(direction.x, 0, direction.y) * speed * Time.deltaTime;
-        PlayerRb.AddForce(direction * speed);
+        //print(direction);
+        direction = transform.TransformDirection(direction);
+        PlayerRb.AddForce(direction * speed * Time.deltaTime, ForceMode.Force);
+        //haha u gay
+
     }
 
 
+    void LateUpdate()
+    {
+        float cameraYRotation = camRot.eulerAngles.y;
+
+        // Apply the camera's Y rotation to the player
+        transform.rotation = Quaternion.Euler(0, cameraYRotation, 0);
+    }
 
     private void FixedUpdate()
     {
-        
+
     }
 }
