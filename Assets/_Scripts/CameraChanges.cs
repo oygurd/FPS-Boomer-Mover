008c3744f75780731ffee0a789cf4a5589a79b62
oygurd <nir2939@gmail.com>
2024-05-21 22:44:49 +0300

    started adding fov change

---
+++ Assets/_Scripts/CameraChanges.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraChanges : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            Camera.main.fieldOfView = 110;
+        }
+
+    }
+}
