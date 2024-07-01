755acdab5d2ca67ad9c2a720cb3c72dc81c8fdea
oygurd <86951294+oygurd@users.noreply.github.com>
2024-07-01 14:48:04 +0300

    almost done with animations

--- Assets/-Scripts/Animations Scripts/ShotgunAnim.cs
+++ Assets/-Scripts/Animations Scripts/ShotgunAnim.cs
@@ -8,7 +8,6 @@ public class ShotgunAnim : MonoBehaviour
     //also the hands animations here!!!//
 
     Animator shotgunAnims;
-    public Animator HandsAnims;
     public ShotgunMechanic shotgunScript;
 
     public TMP_Text AmmoCountUI;
@@ -24,11 +23,11 @@ public class ShotgunAnim : MonoBehaviour
     void Update()
     {
         AmmoCountUI.text = shotgunScript.Ammo.ToString();
-        
+
+
         if (Input.GetKeyDown(KeyCode.Mouse0) && shotgunScript.Ammo > 0 && shotgunScript.canShoot == true)
         {
-            shotgunAnims.SetBool("WeaponShoot", true);
-            HandsAnims.SetBool("handShoot", true);
+            shotgunAnims.SetBool("shoot", true);
             /*if(shotgunScript.Ammo == 0)
             {
                 shotgunAnims.SetBool("OutOfAmmo", true);
@@ -36,13 +35,25 @@ public class ShotgunAnim : MonoBehaviour
                 shotgunAnims.SetBool("Idle", false);
             }*/
         }
-
-        if (shotgunScript.Ammo == 0 && shotgunScript.Ammo != shotgunScript.maxAmmo  || Input.GetKeyDown(KeyCode.R))
+        else
         {
-            shotgunAnims.SetBool("WeaponReload", true);
-            HandsAnims.SetTrigger("HandsReload");
-            shotgunAnims.SetBool("Idle", false);
+            shotgunAnims.SetBool("shoot", false);
+
+        }
+
+        if (shotgunScript.Ammo == 0 && shotgunScript.Ammo != shotgunScript.maxAmmo && shotgunAnims.GetCurrentAnimatorStateInfo(1).IsName("Idle") || Input.GetKeyDown(KeyCode.R) )
+        {
+           
+            shotgunAnims.SetBool("reload", true);
+
+
             shotgunScript.canShoot = false;
+
+        }
+        else
+        {
+            shotgunAnims.SetTrigger("lever");
+            shotgunAnims.SetTrigger("EndLoad");
 
         }
 
@@ -52,18 +63,15 @@ public class ShotgunAnim : MonoBehaviour
     }
 
     public void IdleWeapon()
-    {     
+    {
         shotgunScript.canShoot = true;
 
-        shotgunAnims.SetBool("WeaponReload", false);
-        shotgunAnims.SetBool("WeaponShoot", false);
-        shotgunAnims.SetBool("HandsReload", false);
+        shotgunAnims.SetTrigger("EndLoad");
+        //shotgunAnims.SetBool("reload", false);
 
-
-        shotgunAnims.SetBool("Idle", true);
-        shotgunScript.Ammo = shotgunScript.maxAmmo;
+        // shotgunScript.Ammo = shotgunScript.maxAmmo;
     }
-    
+
     public void CantShoot()
     {
         shotgunScript.canShoot = false;
@@ -73,21 +81,22 @@ public class ShotgunAnim : MonoBehaviour
     public void CanShoot()
     {
         shotgunScript.canShoot = true;
-        HandsAnims.SetBool("handShoot", false);
 
     }
 
 
     public void ActivateLeverPull()
     {
-        shotgunAnims.SetBool("WeaponShoot", false);
-
+        shotgunAnims.SetTrigger("lever");
     }
 
     public void ReloadBullets()
     {
         shotgunScript.Ammo = 2;
         AmmoCountUI.text = shotgunScript.Ammo.ToString();
+        shotgunAnims.SetBool("reload", false);
+        //shotgunAnims.SetTrigger("EndLoad");
+
 
     }
 }
