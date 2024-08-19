4d1b00681b97a49ec6ab59d1bcf56d98df2a4f28
oygurd <nir2939@gmail.com>
2024-08-19 11:31:36 +0300

    Weapon switching system, working on grenade launcher animations

--- Assets/-Scripts/weapons/GrenadeLauncherScript.cs
+++ Assets/-Scripts/weapons/GrenadeLauncherScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:46ebef618b8d257c83bf974b54b0bf62ab0968d0dcc4ed234fd2a298ecb05624
-size 901
+oid sha256:fd07fb38b8e009868ed330ba246aabf36e0cfff679a577e2a3247f89e5811362
+size 915
---
+++ Assets/-Scripts/weapons/WeaponHolder.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:39120ea3861c7f1bc14189c69d9817f5de5745ea7b3f282fc24b0a1e3466dd83
+size 1910
