6ae5639ff3787499bf8c968995e417c0a1ecd52d
oygurd <86951294+oygurd@users.noreply.github.com>
2024-08-28 17:30:20 +0300

    Improve enemy AI,  menu screen changes
    Make the enemy AI use the nav msh system!

--- Assets/-Scripts/Checkpoints/StopTimerGoNextLevel.cs
+++ Assets/-Scripts/Checkpoints/StopTimerGoNextLevel.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d354643f879dd3e312e8abe3bbf85a97ef1c3cfe1ea921af66cc029c92a4ee3b
-size 1319
+oid sha256:40721075bd80281d6fbffd40540b531571da501a2bb20def17e753e3d41a4f74
+size 1358
--- Assets/-Scripts/enemy/BallEnemyActions.cs
+++ Assets/-Scripts/enemy/BallEnemyActions.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:acbeb6d10a4f3e3e7e27de0b8958706237ae16096c93062eee63e4c272549c79
-size 2663
+oid sha256:8fb0b7a6c72ffbf5e7cf42830e0a774d86e0761126d2af1d841b90b1f883db11
+size 2558
