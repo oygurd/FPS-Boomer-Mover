1503e53dd982a8c83542b3d617b8bb971046a036
oygurd <86951294+oygurd@users.noreply.github.com>
2024-09-02 01:12:00 +0300

    Reworking main map, make it better

--- Assets/-Scripts/enemy/BallEnemyActions.cs
+++ Assets/-Scripts/enemy/BallEnemyActions.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8fb0b7a6c72ffbf5e7cf42830e0a774d86e0761126d2af1d841b90b1f883db11
-size 2558
+oid sha256:ccbd837056bc447e5cc5eb437b8fc1a32b4b0557a25894592fe6d70d64d082d3
+size 2554
---
+++ Assets/-Scripts/enemy/NewBallEnemyActions.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9c347147b1d29ca495ae72c3ab107609d0e1a347a91775a3e8fb129d4c1800fe
+size 2671
