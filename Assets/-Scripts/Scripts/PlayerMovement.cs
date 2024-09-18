f4a6b845934c3a31f7133aa9cfa45db274d7a9f4
oygurd <86951294+oygurd@users.noreply.github.com>
2024-09-19 00:05:44 +0300

    small fix double jump

--- Assets/-Scripts/Scripts/PlayerMovement.cs
+++ Assets/-Scripts/Scripts/PlayerMovement.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7deef67cedc79c08fd8031185de437505e573a9a54bd5b52a1faaeaabb8c90b9
-size 6624
+oid sha256:48599aee93958376d0113f4c944e1c8eecf571a1c210b969851411ea974609fd
+size 6617
