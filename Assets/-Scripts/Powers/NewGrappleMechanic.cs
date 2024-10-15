1d15290770481648abbc37933f322f64929bf5e9
oygurd <86951294+oygurd@users.noreply.github.com>
2024-10-15 21:39:41 +0300

    changes cant remember sadly

--- Assets/-Scripts/Powers/NewGrappleMechanic.cs
+++ Assets/-Scripts/Powers/NewGrappleMechanic.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e050b1af91ca8c1e23e9888cf2b43e32ab54560808af9650e5d71a3764c9dd6c
-size 2461
+oid sha256:944658b33c87ef736f233f0bdbcf3712ad290eb97acdfadc6a698115631af393
+size 2549
